3cc93b8246a9bcfc60bd1908f30250b7f63795f7
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-01-18 17:12:02 +1100

    [MASTER] Set up forms for email names, mobile

--- src/components/TodolenderHome/Navbar/ProfileSettings.tsx
+++ src/components/TodolenderHome/Navbar/ProfileSettings.tsx
@@ -43,15 +43,141 @@ const ProfileSettingsPortal = () => {
         }
     }
 
+    const [editingEmailForm, setEditingEmailForm] = useState(false);
+    const [email, setEmail] = useState(dummyData.email)
+
+    const [editingFirstNameForm, setEditingFirstNameForm] = useState(false);
+    const [firstName, setFirstName] = useState(dummyData.firstName)
+
+    const [editingLastNameForm, setEditingLastNameForm] = useState(false);
+    const [lastName, setLastName] = useState(dummyData.lastName)
+
+    const [editingMobileForm, setEditingMobileForm] = useState(false);
+    const [mobile, setMobile] = useState(dummyData.mobile)
+
+    const [editingPlanReminderForm, setEditingPlanReminderForm] = useState(false);
+    const [planReminder, setPlanReminder] = useState(dummyData.planReminder)
+
+    const [deleteAccountActive, setDeleteAccountActive] = useState(false);
+
+
+    const handleEditEmail = () => setEditingEmailForm(true)
+    const handleEmailChange = (event: { target: { value: React.SetStateAction<string> } }) => setEmail(event.target.value)
+    const cancelEditEmail = () => setEditingEmailForm(false)
+    const handleSaveEditEmail = (event: { preventDefault: () => void }) => {
+        event.preventDefault()
+        console.log(email)
+        // api call to save and update email
+        // re-fetch data
+
+        cancelEditEmail();
+    }
+
+    const handleEditFirstName = () => setEditingFirstNameForm(true)
+    const handleFirstNameChange = (event: { target: { value: React.SetStateAction<string> } }) => setFirstName(event.target.value)
+    const cancelEditFirstName = () => setEditingFirstNameForm(false)
+    const handleSaveEditFirstName = (event: { preventDefault: () => void }) => {
+        event.preventDefault()
+        // api call to save and update email
+        // re-fetch data
+
+        cancelEditFirstName();
+    }
+
+    const handleEditLastName = () => setEditingLastNameForm(true)
+    const handleLastNameChange = (event: { target: { value: React.SetStateAction<string> } }) => setLastName(event.target.value)
+    const cancelEditLastName = () => setEditingLastNameForm(false)
+    const handleSaveEditLastName = (event: { preventDefault: () => void }) => {
+        event.preventDefault()
+        // api call to save and update email
+        // re-fetch data
+
+        cancelEditLastName();
+    }
+
+    const handleEditMobile = () => setEditingMobileForm(true)
+    const handleMobileChange = (event: { target: { value: React.SetStateAction<string> } }) => setMobile(event.target.value)
+    const cancelEditMobile = () => setEditingMobileForm(false)
+    const handleSaveEditMobile= (event: { preventDefault: () => void }) => {
+        event.preventDefault()
+        // api call to save and update email
+        // re-fetch data
+
+        cancelEditMobile();
+    }
+
     return ReactDOM.createPortal(
         <div className="ProfileSettingsMenu Border">
-            <button>Edit Email</button><br />
-            <button>Edit First Name</button><br />
-            <button>Edit Last Name</button><br />
-            <button>Edit Mobile</button><br />
-            <button>Edit Plan Reminder</button><br />
-            <button>Delete Account</button><br />
-            <br /><br />
+            <div className="ProfileSettingsTop">
+                <div className="ProfileSettingsSection">
+                    { editingEmailForm ?
+                        <>
+                            <form onSubmit={handleSaveEditEmail}>
+                                <input type="text" name="email" onChange={handleEmailChange} placeholder="Email"/>
+                                <input type="submit" value="save" />
+                            </form>
+                            <button onClick={cancelEditEmail}>Cancel</button>
+                        </> :
+                        <>
+                            {email}
+                            <button onClick={handleEditEmail}>Edit</button>
+                        </>
+                    }
+                </div>
+                <div className="ProfileSettingsSection">
+                    { editingFirstNameForm ?
+                        <>
+                            <form onSubmit={handleSaveEditFirstName}>
+                                <input type="text" name="email" onChange={handleFirstNameChange} placeholder="First Name"/>
+                                <input type="submit" value="save" />
+                            </form>
+                            <button onClick={cancelEditFirstName}>Cancel</button>
+                        </> :
+                        <>
+                            {firstName}
+                            <button onClick={handleEditFirstName}>Edit</button>
+                        </>
+                    }
+                </div>
+                <div className="ProfileSettingsSection">
+                    { editingLastNameForm ?
+                        <>
+                            <form onSubmit={handleSaveEditLastName}>
+                                <input type="text" name="email" onChange={handleLastNameChange} placeholder="Last Name"/>
+                                <input type="submit" value="save" />
+                            </form>
+                            <button onClick={cancelEditLastName}>Cancel</button>
+                        </> :
+                        <>
+                            {lastName}
+                            <button onClick={handleEditLastName}>Edit</button>
+                        </>
+                    }
+                </div>
+                <div className="ProfileSettingsSection">
+                    { editingMobileForm ?
+                        <>
+                            <form onSubmit={handleSaveEditMobile}>
+                                <input type="text" name="email" onChange={handleMobileChange} placeholder="Email"/>
+                                <input type="submit" value="save" />
+                            </form>
+                            <button onClick={cancelEditMobile}>Cancel</button>
+                        </> :
+                        <>
+                            {mobile}
+                            <button onClick={handleEditMobile}>Edit</button>
+                        </>
+                    }
+                </div>
+                <div className="ProfileSettingsSection">
+                    &nbsp;
+                    <button>Edit Plan Reminder</button>
+                </div>
+                <div className="ProfileSettingsSection">
+                    &nbsp;
+                    <button>Delete Account</button>
+                </div>
+            </div>
             <button>Logout</button>
         </div>,
         // @ts-ignore
