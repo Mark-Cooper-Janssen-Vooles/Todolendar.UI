e1d59a041cefd0e64041aa0facb219b81033a8dc
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-01-27 15:09:42 +1100

    [MASTER] adding todo item form done

--- src/components/TodolenderHome/Todolist.tsx
+++ src/components/TodolenderHome/Todolist.tsx
@@ -4,18 +4,22 @@ import './Todolist.css'
 
 const todos = [
     {
+        id: 1,
         title: 'Todo item 1',
         description: 'To do item 1 description'
     },
     {
+        id: 2,
         title: 'Todo item 2',
         description: 'To do item 2 description'
     },
     {
+        id: 3,
         title: 'Todo item 3',
         description: 'To do item 3 description'
     },
     {
+        id: 4,
         title: 'Todo item 4',
         description: 'To do item 4 description'
     }
@@ -24,6 +28,31 @@ const todos = [
 
 const Todolist = () => {
     const [addActive, setAddActive] = useState(false)
+    const [addDate, setAddDate] = useState('')
+
+    const handleAdd = () => {
+        setAddActive(!addActive)
+    }
+
+    const handleAddDate = (e: any) => {
+        e.preventDefault()
+        setAddDate(e.target.value)
+    }
+
+    const handleSaveAdd = (e: any) => {
+        e.preventDefault()
+        console.log(addDate)
+        // api call to add to calendar
+        // re-fetch data
+
+        setAddActive(false)
+    }
+
+    const StandardButtons = () => <>
+        <button onClick={handleAdd}>Add</button>
+        <button>Edit</button>
+        <button>Delete</button>
+    </>
 
     return (
         <div className="Todolist Border">
@@ -31,21 +60,20 @@ const Todolist = () => {
             <ul className="TodolistUl">
                 {todos?.map((todo) => {
                     return (
-                    <li className="TodolistLi">
+                    <li key={todo.id} className="TodolistLi">
                         <div className="TodoListLiTitle">{todo.title} </div>
                         {todo.description && <div className="TodolistLiDescription">{todo.description}</div>}
 
                         { addActive ?
                             <>
-                                <div>Add form</div>
-                                <button>Cancel</button>
+                                <form onSubmit={handleSaveAdd}>
+                                    <input type="date" onChange={handleAddDate}/>
+                                    <input type="submit" value="save" />
+                                </form>
+                                <button onClick={handleAdd}>Cancel</button>
                             </>
                             :
-                            <>
-                                <button>Add</button>
-                                <button>Edit</button>
-                                <button>Delete</button>
-                            </>
+                            <StandardButtons />
                         }
                     </li>
                     )
