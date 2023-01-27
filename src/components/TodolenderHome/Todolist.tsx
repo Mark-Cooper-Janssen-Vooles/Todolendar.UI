8283dea47c62eec4d607f1a0ccfa2117a5f37c49
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-01-27 15:54:18 +1100

    [MASTER] Finish todolist forms and buttons

--- src/components/TodolenderHome/Todolist.tsx
+++ src/components/TodolenderHome/Todolist.tsx
@@ -25,13 +25,16 @@ const todos = [
     }
 ]
 
-
 const Todolist = () => {
     const [addActive, setAddActive] = useState(false)
     const [addDate, setAddDate] = useState('')
 
-    const handleAdd = () => {
+    const [editTodo, setEditTodo] = useState(false)
+    const [activeTodo, setActiveTodo] = useState({ id: 0, title: '', description: ''})
+
+    const handleAdd = (todo: React.SetStateAction<{ id: number; title: string; description: string; }>) => {
         setAddActive(!addActive)
+        setActiveTodo(todo)
     }
 
     const handleAddDate = (e: any) => {
@@ -48,33 +51,91 @@ const Todolist = () => {
         setAddActive(false)
     }
 
-    const StandardButtons = () => <>
-        <button onClick={handleAdd}>Add</button>
-        <button>Edit</button>
-        <button>Delete</button>
-    </>
+    const handleEditTodo = (todo: React.SetStateAction<{ id: number; title: string; description: string; }>) => {
+        setEditTodo(!editTodo)
+        setActiveTodo(todo)
+    }
+
+    const handleEditedTodo = (e: any) => {
+        e.preventDefault()
+
+        console.log(e.target.type)
+
+        if (e.target.type == "text") {
+            console.log(e.target.value)
+            setActiveTodo({
+                id: activeTodo.id,
+                title: e.target.value,
+                description: activeTodo.description
+            })
+        }
+
+        if (e.target.type == "textarea") {
+            console.log(e.target.value)
+            setActiveTodo({
+                id: activeTodo.id,
+                title: activeTodo.title,
+                description: e.target.value
+            })
+        }
+    }
+
+    const handleSaveEditTodo = (e: any) => {
+        e.preventDefault()
+        console.log(activeTodo)
+        // api call to edit todo
+        // re-fetch data
+
+        setEditTodo(false)
+    }
+
+    const handleDeleteTodo = (todo: React.SetStateAction<{ id: number; title: string; description: string; }>) => {
+        window.alert('You have deleted the todo!')
+        // api call to delete todo
+    }
+
+    const StandardButtons = (props: { currentTodo: { id: number, title: string, description: string }}) => <>
+            <button onClick={() => handleAdd(props.currentTodo)}>Add</button>
+            <button onClick={() => handleEditTodo(props.currentTodo)}>Edit</button>
+            <button onClick={() => handleDeleteTodo(props.currentTodo)}>Delete</button>
+        </>
 
     return (
         <div className="Todolist Border">
             <div className="TodoTitle">Todo List</div>
             <ul className="TodolistUl">
                 {todos?.map((todo) => {
+                    const currentTodo = todos.find(x => x.id == todo.id)
+                    const currentTodoAddActiveForm = addActive && todo.id === activeTodo.id
+                    const currentTodoEditTodoActive = editTodo && todo.id === activeTodo.id;
+                    const formActiveForCurrentTodo = currentTodoAddActiveForm || currentTodoEditTodoActive
+
                     return (
                     <li key={todo.id} className="TodolistLi">
                         <div className="TodoListLiTitle">{todo.title} </div>
                         {todo.description && <div className="TodolistLiDescription">{todo.description}</div>}
 
-                        { addActive ?
+                        { currentTodoAddActiveForm &&
                             <>
                                 <form onSubmit={handleSaveAdd}>
                                     <input type="date" onChange={handleAddDate}/>
                                     <input type="submit" value="save" />
                                 </form>
-                                <button onClick={handleAdd}>Cancel</button>
-                            </>
-                            :
-                            <StandardButtons />
-                        }
+                                <button onClick={() => setAddActive(false)}>Cancel</button>
+                            </> }
+
+                        { currentTodoEditTodoActive &&
+                            <>
+                                <form onSubmit={handleSaveEditTodo}>
+                                    {/*// @ts-ignore*/}
+                                    <input type="text" name="title" onChange={handleEditedTodo} defaultValue={currentTodo.title}/>
+                                    {/*// @ts-ignore*/}
+                                    <textarea name="description" onChange={handleEditedTodo} defaultValue={currentTodo.description} />
+                                    <input type="submit" value="save" />
+                                </form>
+                                <button onClick={() => setEditTodo(false)}>Cancel</button>
+                            </> }
+                        { !formActiveForCurrentTodo && <StandardButtons currentTodo={todo} /> }
                     </li>
                     )
                 })}
