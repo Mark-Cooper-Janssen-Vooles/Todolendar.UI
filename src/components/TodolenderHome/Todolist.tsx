def778fd2dfd71c11748bdff41177f8e9e5eb736
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-01-27 14:12:09 +1100

    [MASTER] Add styling and dummy data for todolist

--- src/components/TodolenderHome/Todolist.tsx
+++ src/components/TodolenderHome/Todolist.tsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../App.css';
 import './Todolist.css'
 
+const todos = [
+    {
+        title: 'Todo item 1',
+        description: 'To do item 1 description'
+    },
+    {
+        title: 'Todo item 2',
+        description: 'To do item 2 description'
+    },
+    {
+        title: 'Todo item 3',
+        description: 'To do item 3 description'
+    },
+    {
+        title: 'Todo item 4',
+        description: 'To do item 4 description'
+    }
+]
+
+
 const Todolist = () => {
+    const [addActive, setAddActive] = useState(false)
+
     return (
-        <div className="Todolist Border">todo list</div>
+        <div className="Todolist Border">
+            <div className="TodoTitle">Todo List</div>
+            <ul className="TodolistUl">
+                {todos?.map((todo) => {
+                    return (
+                    <li className="TodolistLi">
+                        <div className="TodoListLiTitle">{todo.title} </div>
+                        {todo.description && <div className="TodolistLiDescription">{todo.description}</div>}
+
+                        { addActive ?
+                            <>
+                                <div>Add form</div>
+                                <button>Cancel</button>
+                            </>
+                            :
+                            <>
+                                <button>Add</button>
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </>
+                        }
+                    </li>
+                    )
+                })}
+            </ul>
+        </div>
     )
 }
 
