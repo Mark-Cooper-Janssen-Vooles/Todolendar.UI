d279220bb948ee0ac5062106c3ad2a06207687ef
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-02-15 18:51:12 +1100

    [MASTER] Add forms for all but scheduled at

--- src/components/TodolenderHome/Calendar/ScheduledTodoPortal.tsx
+++ src/components/TodolenderHome/Calendar/ScheduledTodoPortal.tsx
@@ -30,6 +30,10 @@ const ScheduledTodoPortal = ({ setScheduledTodoOpen, activeScheduledTodo }: ISch
     const [title, setTitle] = useState(activeScheduledTodo.Title)
     const [editingDescriptionForm, setEditingDescriptionForm] = useState(false)
     const [description, setDescription] = useState(activeScheduledTodo.Description)
+    const [editingNotifyTimeForm, setEditingNotifyTimeForm] = useState(false)
+    const [notifyTime, setNotifyTime] = useState(activeScheduledTodo.NotifyBeforeTime.toString())
+    const [editingColourForm, setEditingColourForm] = useState(false)
+    const [colour, setColour] = useState(activeScheduledTodo.Colour)
 
     const handleEditTitle = () => setEditingTitleForm(true)
     const handleTitleChange = (event: { target: { value: React.SetStateAction<string> } }) => setTitle(event.target.value)
@@ -53,6 +57,27 @@ const ScheduledTodoPortal = ({ setScheduledTodoOpen, activeScheduledTodo }: ISch
         cancelEditDescription();
     }
 
+    const handleEditNotifyTime = () => setEditingNotifyTimeForm(true)
+    const handleEditNotifyTimeChange = (event: { target: { value: React.SetStateAction<string> } }) => setNotifyTime(event.target.value)
+    const cancelEditNotifyTime = () => setEditingNotifyTimeForm(false)
+    const handleSaveNotifyTime = (event: { preventDefault: () => void }) => {
+        event.preventDefault()
+        console.log(description)
+        // api call to save and update email
+        // re-fetch data
+        cancelEditNotifyTime();
+    }
+
+    const handleEditColourForm = () => setEditingColourForm(true)
+    const handleEditColourFormChange = (event: { target: { value: React.SetStateAction<string> } }) => setColour(event.target.value)
+    const cancelEditColourForm = () => setEditingColourForm(false)
+    const handleSaveColourForm = (event: { preventDefault: () => void }) => {
+        event.preventDefault()
+        console.log(description)
+        // api call to save and update email
+        // re-fetch data
+        cancelEditColourForm();
+    }
 
     return ReactDOM.createPortal(
         <div className="ScheduledTodoPortalContainer Border">
@@ -89,11 +114,40 @@ const ScheduledTodoPortal = ({ setScheduledTodoOpen, activeScheduledTodo }: ISch
                 }
             </div>
 
-            <div>Notify Before Time: {activeScheduledTodo.NotifyBeforeTime} minutes</div>
-            <div>Created at: {activeScheduledTodo.CreatedAt}</div>
-            <div>Updated at: {activeScheduledTodo.UpdatedAt}</div>
+            <div className="ScheduledTodoPortalSection">
+                { editingNotifyTimeForm ?
+                    <>
+                        <form onSubmit={handleSaveNotifyTime}>
+                            <label htmlFor="title">Notify Before Time (minutes):</label>
+                            <input type="number" name="title" onChange={handleEditNotifyTimeChange} placeholder={`${notifyTime}`}/>
+                            <input type="submit" value="save" />
+                        </form>
+                        <button onClick={cancelEditNotifyTime}>Cancel</button>
+                    </> :
+                    <>
+                        Notify Before Time: {notifyTime} minutes
+                        <button onClick={handleEditNotifyTime}>Edit</button>
+                    </>
+                }
+            </div>
+
+            <div className="ScheduledTodoPortalSection">
+                { editingColourForm ?
+                    <>
+                        <form onSubmit={handleSaveColourForm}>
+                            <input type="color" name="title" onChange={handleEditColourFormChange} placeholder={`${colour}`}/>
+                            <input type="submit" value="save" />
+                        </form>
+                        <button onClick={cancelEditColourForm}>Cancel</button>
+                    </> :
+                    <>
+                        Colour: {colour}
+                        <button onClick={handleEditColourForm}>Edit</button>
+                    </>
+                }
+            </div>
+
             <div>Scheduled at: {activeScheduledTodo.ScheduledAt}</div>
-            <div>Colour: {activeScheduledTodo.Colour}</div>
         </div>,
         // @ts-ignore
         document.body
