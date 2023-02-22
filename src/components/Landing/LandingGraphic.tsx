40d3a61103311785ed79ce8bdf4d6de0217484d2
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-02-22 16:02:29 +1100

    [MASTER] Add logo and fix warnings about flex-start

--- src/components/Landing/LandingGraphic.tsx
+++ src/components/Landing/LandingGraphic.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './LandingGraphic.css';
 import '../../App.css'
+import todolenderLogo from "../../logo.png";
 
 const LandingGraphic = () => {
-    return <div className="LandingGraphic Border">Landing Graphic Area</div>
+    return (
+        <div className="LandingGraphic Border">
+            <img src={todolenderLogo} alt="Todolender logo" />
+
+        </div>
+    )
 }
 
 export default LandingGraphic;
